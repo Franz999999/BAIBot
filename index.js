8edaf880aef350317c85110d6029776b965d68db
Franz999999 <141624895+Franz999999@users.noreply.github.com>
2023-11-25 14:02:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ const initBot = () => {
 
 const getChatGptResponse = async (question) => {
   try {
-    const apiKey = "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";
+    const apiKey = "";
     const response = await axios.post(
       "https://api.openai.com/v1/chat/completions",
       {
